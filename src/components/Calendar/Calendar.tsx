712ed1661bff685b3589d0a35c379738652e0f11
LaptopNickolas <panteleew.kolya@gmail.com>
2024-07-18 15:28:00 +0900

    modified:   src/components/Calendar/calendar.css

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -94,7 +94,10 @@ export const Calendar: React.FC<CalendarProps> = ({
             )}
             <button className="calendar__menu-button" onClick={() => {functions.onClickArrow('right')}}>{">"}</button>
           </div>
-          <ul className="calendar__table list-style">
+
+            {state.mode === "days" && (
+              <ul className="calendar__table list-style">
+
             {state.mode === "days" &&
               state.weekDaysNames.map((weekDaysName) => (
                 <li className="calendar__week-days" key={weekDaysName.dayShort}>
@@ -102,47 +105,106 @@ export const Calendar: React.FC<CalendarProps> = ({
                 </li>
               ))}
 
-            {state.mode === "days" &&
-              state.calendarDays.map((day) => {
-                const isToday = checkIsToday(day.date);
-                const isSelectedDay = checkDateIsEqual(
-                  day.date,
-                  state.selectedDate.date
-                );
-                const isAdditionalDay =
-                  day.monthIndex !== state.selectedMonth.monthIndex;
-                const isWeekend =
-                  day.dayShort === "сб" || day.dayShort === "вс";
-                const underlining = day.timestamp <=  Date.now() ? 'calendar__day_underlining' : '';
-                const underliningGreen = greenLine.includes(day.timestamp);
-                const underliningYellow = yellowLine.includes(day.timestamp);
-                return (
-                  <li
-                    className={`calendar__day 
-                ${isToday ? " calendar__today" : ""}
-                ${isSelectedDay ? " calendar__today" : ""}
-                ${isWeekend ? " calendar__weekend" : ""}
-                ${isAdditionalDay ? " calendar__additional-day" : ""}
-                ${underlining ? " calendar__day_underlining" : ""}
-                ${underliningGreen  ? ' calendar__day_underlining_green' : ''}
-                ${underliningYellow ? ' calendar__day_underlining_yellow' : ''}
-                `}
-                    key={day.timestamp}
-                    onClick={() => {
-                      if(day.timestamp <= Date.now()) {
-                        functions.setSelectedDate(day);
-                      }
-                    }}
-                  >
-                    {day.dayNumber}
-                  </li>
-                );
+
+              { state.calendarDays.map((day) => {
+                 const isToday = checkIsToday(day.date);
+                 const isSelectedDay = checkDateIsEqual(
+                   day.date,
+                   state.selectedDate.date
+                 );
+                 const isAdditionalDay =
+                   day.monthIndex !== state.selectedMonth.monthIndex;
+                 const isWeekend =
+                   day.dayShort === "сб" || day.dayShort === "вс";
+                 const underlining = day.timestamp <=  Date.now() ? 'calendar__day_underlining' : '';
+                 const underliningGreen = greenLine.includes(day.timestamp);
+                 const underliningYellow = yellowLine.includes(day.timestamp);
+                 return (
+                   <li
+                     className={`calendar__day 
+                 ${isToday ? " calendar__today" : ""}
+                 ${isSelectedDay ? " calendar__selected-day" : ""}
+                 ${isWeekend ? " calendar__weekend" : ""}
+                 ${isAdditionalDay ? " calendar__additional-day" : ""}
+                 ${underlining ? " calendar__day_underlining" : ""}
+                 ${underliningGreen  ? ' calendar__day_underlining_green' : ''}
+                 ${underliningYellow ? ' calendar__day_underlining_yellow' : ''}
+                 `}
+                     key={day.timestamp}
+                     onClick={() => {
+                       if(day.timestamp <= Date.now()) {
+                         functions.setSelectedDate(day);
+                       }
+                     }}
+                   >
+                     {day.dayNumber}
+                   </li>
+                 );
+               })}
+             </ul>
+ 
+            )}
+
+          {state.mode === 'monthes' && (
+            <ul className="list-style calendar__pick__items__container">
+              {state.monthesNames.map(monthesName => {
+                const isCurrentMonth =
+                  new Date().getMonth() === monthesName.monthIndex && 
+                  new Date().getFullYear() === state.selectedYear;
+                const isSelectedMonth = monthesName.monthIndex === state.selectedMonth.monthIndex
+
+                return <li key={monthesName.month} className={["calendar__month",
+                  isCurrentMonth ? 'calendar__today' : '',
+                  isSelectedMonth ? 'calendar__selected-day' : ''
+                ].join(' ')}
+                onClick={() => {
+                  functions.setMode('days');
+                  functions.setSelectedMonthByIndex(monthesName.monthIndex);
+                }}
+                >{monthesName.monthShort}</li>
               })}
-          </ul>
+            </ul>
+          )}
+
+        {state.mode === 'years' && (
+          <div className='calendar__pick__items__container'>
+            <div className='calendar__unchoosable__year'>{state.selectedYearsInterval[0] - 1}</div>
+            {state.selectedYearsInterval.map((year) => {
+              console.log(year)
+              const isCurrentYear = new Date().getFullYear() === year;
+              const isSelectedYear = year === state.selectedYear;
+
+              return (
+                <div
+                  key={year}
+                  aria-hidden
+                  onClick={() => {
+                    functions.setSelectedYear(year);
+                    functions.setMode('monthes');
+                  }}
+                  className={[
+                    'calendar__pick__item',
+                    isCurrentYear ? 'calendar__today' : '',
+                    isSelectedYear ? 'calendar__selected-day' : ''
+                  ].join(' ')}
+                >
+                  {year}
+                </div>
+              );
+            })}
+            <div className='calendar__unchoosable__year'>
+              {state.selectedYearsInterval[state.selectedYearsInterval.length - 1] + 1}
+            </div>
+          </div>
+        )}
+
+
+
           <button className="calendar__choose-button" onClick={() => {
             selectDate(state.selectedDate.date);
             toggleOpenCalendarHandler();
           }}>Выбрать</button>
+
         </div>
       </div>
     );
